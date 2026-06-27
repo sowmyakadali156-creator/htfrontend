fea43785ed14fe3a7562e5928857a84ee4c0d377
Sowmya Kadali <sowmyakadali156@gmail.com>
2026-06-27 19:34:26 +0530

    cors issue fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_URL || "https://habittracker-of6r.onrender.com/api";
+
 export const API = axios.create({
-  baseURL: "https://habittracker-of6r.onrender.com/api"
+  baseURL,
 });
 
 API.interceptors.request.use((req) => {
